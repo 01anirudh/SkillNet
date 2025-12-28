c4860915593348653e8f7e815afaeee2b9cc9e83
01Anirudh <anirudhkandwal123@gmail.com>
2025-12-28 23:20:53 +0530

    debugging

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import 'dotenv/config';
 import connectDB from './configs/db.js';
 import { serve } from "inngest/express";
 import {inngest,functions} from './inngest/index.js'
-import { clerkMiddleware } from '@clerk/express'
+// import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 
@@ -12,7 +12,7 @@ await connectDB();
 
 app.use(express.json());
 app.use(cors());
-app.use(clerkMiddleware);
+// app.use(clerkMiddleware);
 
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
