ce9fa81cfb70e35bfeeda0a26c0870cad8ce33fd
01Anirudh <anirudhkandwal123@gmail.com>
2025-12-28 16:51:36 +0530

    correctiong moongoose modal

--- server/models/user.js
+++ server/models/user.js
@@ -8,9 +8,9 @@ const userSchema = new mongoose.Schema({
     bio:{type:String,default:'Hey there! I am using SkillNet'},
     profile_picture:{type:String,default:''},
     cover_photo:{type:String,default:''},
-    location:{type:String,default:''},
-    followers:{type:String,ref:'User'},
-    following:{type:String,ref:'User'},
+    location:[{type:String,default:''}],
+    followers:[{type:String,ref:'User'}],
+    following:[{type:String,ref:'User'}],
     connections:{type:String,ref:'User'},
 },{timestamps:true,minimize:false})
 
