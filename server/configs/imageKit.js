ce0af92a72a71f723eb6e2272de6121d64304ec9
01Anirudh <anirudhkandwal123@gmail.com>
2025-12-31 01:33:16 +0530

    correcting imageKit

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -1,10 +1,16 @@
 import ImageKit from '@imagekit/nodejs';
 
 
+if (!process.env.IMAGEKIT_PRIVATE_KEY) {
+  console.error('Error: IMAGEKIT_PRIVATE_KEY is missing in environment variables');
+}
+
 const imageKit = new ImageKit({
-      privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-    });
- 
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
+});
+
 
 
 
