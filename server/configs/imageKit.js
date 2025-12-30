d22719b0a2c2b5425adec58d721e5ba0a0cc20fc
01Anirudh <anirudhkandwal123@gmail.com>
2025-12-31 01:20:48 +0530

    handling imageKit

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -19,27 +19,11 @@ let imageKit;
 
 
     imageKit = new ImageKit({
-      publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
       privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-      urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
     });
  
 
 
-// URL with basic transformations
-// export const transformedUrl = client.helper.buildSrc({
-//   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
-//   src: '/path/to/image.jpg',
-//   transformation: [
-//     {
-//       width: 400,
-//       height: 300,
-//       crop: 'maintain_ratio',
-//       quality: 80,
-//       format: 'webp',
-//     },
-//   ],
-// });
-// Result: https://ik.imagekit.io/your_imagekit_id/path/to/image.jpg?tr=w-400,h-300,c-maintain_ratio,q-80,f-webp
+
 
 export default imageKit;
