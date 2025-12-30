3b293b96ddc2825bdd7951926753734db386fcfc
01Anirudh <anirudhkandwal123@gmail.com>
2025-12-31 01:26:28 +0530

    correcting imageKit

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -1,25 +1,8 @@
-// import ImageKit from '@imagekit/nodejs';
-
-// const imageKit = new ImageKit({
-//   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-//   privateKey: process.env.IMAGEKIT_PRIVATE_KEY, // This is the default and can be omitted
-//   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
-// });
-
-import 'dotenv/config';
 import ImageKit from '@imagekit/nodejs';
 
-// const imageKit = new ImageKit({
-//   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-//   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-//   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
-// });
 
-let imageKit;
-
-
-    imageKit = new ImageKit({
-      privateKey: process.env['IMAGEKIT_PRIVATE_KEY'],
+const imageKit = new ImageKit({
+      privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
     });
  
 
