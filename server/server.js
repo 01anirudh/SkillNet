d1f47c2fff92670d558483e345321f7dd129736e
01Anirudh <anirudhkandwal123@gmail.com>
2026-01-01 20:02:56 +0530

    cors secure

--- server/server.js
+++ server/server.js
@@ -15,16 +15,19 @@ const app = express();
 await connectDB();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    credentials: true
+}));
 app.use(clerkMiddleware());
 
 
 app.get('/', (req, res) => res.send('server is running'))
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use('/api/user', userRouter);
-app.use('/api/post',postRouter);
-app.use('/api/story',storyRouter);
-app.use('/api/message',messageRouter);
+app.use('/api/post', postRouter);
+app.use('/api/story', storyRouter);
+app.use('/api/message', messageRouter);
 
 
 
